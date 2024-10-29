553d3f2bb4b53748524eb4444123b96da2a1b2a9
Stephen C Phillips <steve@scphillips.com>
2024-10-29 21:30:28 +0000

    Report errors and store logs

--- example-models/run.sh
+++ example-models/run.sh
@@ -1,16 +1,27 @@
 #!/bin/sh
 
+# Runs the risk-report.py script on all .nq.gz files in the current directory
+# Assumes the correct domain model is at ~/domain-network/csv
+
+DATETIME=$(date +"%Y%m%d%H%M%S")
+LOGDIR="/tmp/risk-report-${DATETIME}"
+mkdir -p ${LOGDIR}
+
 # Loop through all files ending with .nq.gz in the current directory
 for FILE in *.nq.gz; do
     # Extract the base name of the file (without the extension)
     BASENAME=$(basename "$FILE" .nq.gz)
-    
-    # Get the current datetime string
-    DATETIME=$(date +"%Y%m%d%H%M%S")
-    mkdir -p "/tmp/${DATETIME}"
-    
-    # Execute the command with the appropriate arguments
-    ../risk-report.py -i "$FILE" -o "/tmp/${DATETIME}/${BASENAME}.csv" -d ~/domain-network/csv
+   
+    # Execute the command with the appropriate arguments and capture stdout and stderr
+    echo "Processing '$FILE'..."
+    LOGFILE="${LOGDIR}/${BASENAME}.log"
+    ../risk-report.py -i "${FILE}" -o "${LOGDIR}/${BASENAME}.csv" -d ~/domain-network/csv > "${LOGFILE}" 2>&1
+    # Check for "Traceback" in the log file and output the relevant lines if found
+    if grep -q "Traceback" "${LOGFILE}"; then
+        echo "\nError found when processing '${FILE}':"
+        grep -A 1000 "Traceback" "${LOGFILE}"
+        echo
+    fi
 done
 
-echo "All files processed successfully. Output is in /tmp/${DATETIME}/"
+echo "Output is in ${LOGDIR}/"
